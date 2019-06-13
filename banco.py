f016abc98700b31559fc9ba3891c40c76a20404f
Leonardo Nunes <leonardovnun@gmail.com>
2019-06-13 09:48:20 -0300

    First Commit

---
+++ banco.py
@@ -0,0 +1,11 @@
+class Leilao:
+    dicionario = {}
+    def __init__(self,item,vmin):
+        self.item = item
+        self.vmin = vmin
+        self.estado = 0
+
+
+
+    def lance(self,nome,valor_lance):
+        self.dicionario[nome] = valor_lance
